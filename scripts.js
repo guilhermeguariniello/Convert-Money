3facc5df3c0fc6a7d6726f3c3bc5e7499a813568
Guiznn013 <guariniellolives@gmail.com>
2025-05-23 04:53:55 -0300

    New coins options to convert money

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
+const currencySelectFrom = document.querySelector(".currency-select-from");
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -7,6 +8,8 @@ function convertValues() {
     const currencyValueConverted = document.querySelector(".currency-value"); // Valor convertido em outras moedas.
     const dolarToday = 5.72;
     const euroToday = 6.47;
+    const libraToday = 7.71;
+    const bitcoinToday = 350000;
 
     if(currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -15,11 +18,32 @@ function convertValues() {
         }).format(inputCurrencyValue / dolarToday);
     }
 
+    if(currencySelect.value == "real") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        }).format(inputCurrencyValue);
+    }
+
     if(currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
         style: "currency",
         currency: "EUR",
         }).format(inputCurrencyValue / euroToday);
+    }
+
+    if(currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+        style: "currency",
+        currency: "GBP",
+        }).format(inputCurrencyValue / libraToday);
+    }
+
+    if(currencySelect.value == "bitcoin") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "BTC",
+        }).format(inputCurrencyValue / bitcoinToday);
     }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -38,11 +62,26 @@ function changeCurrency() {
         currencyImg.src = "./assets/dolar.png";
     }
 
+    if(currencySelect.value == "real") {
+        currencyName.innerHTML = "Real";
+        currencyImg.src = "./assets/real.png";
+    }
+
     if(currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro";
         currencyImg.src = "./assets/euro.png";
     }
-    
+
+    if(currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra";
+        currencyImg.src = "./assets/libra.png";
+    }
+
+    if(currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin";
+        currencyImg.src = "./assets/bitcoin.png";
+    }
+
     convertValues();
 }
 
